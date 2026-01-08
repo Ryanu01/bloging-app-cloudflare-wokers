23fae753568db38a9243c861d441ea71374310fa
Ryan Gomes <ryangomes08090109@gmail.com>
2026-01-08 22:01:30 +0530

    be done

--- backend/src/controler/postControler.ts
+++ backend/src/controler/postControler.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Context } from "hono";
-import { postRouter } from "../routes/postRouter";
-import { id } from "zod/locales";
 import { postSchema } from "../zod/types";
-import { useId } from "hono/jsx";
+import { every } from "hono/combine";
+import { postRouter } from "../routes/postRouter";
+import { is } from "zod/locales";
+import { promise } from "zod";
 
 export const getPosts = async (c: Context) => {
   const prisma = new PrismaClient({
@@ -85,4 +86,132 @@ export const postPosts = async (c: Context) => {
       message: `Error occured ${error}`,
     });
   }
+};
+
+export const getPostsById = async (c: Context) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const userId = c.get("userId");
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId.userId,
+      },
+    });
+
+    if (!user) {
+      return c.body("User not found", 404);
+    }
+
+    const id = parseInt(c.req.param("id")!);
+
+    const posts = await prisma.post.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!posts) {
+      return c.body("No posts available", 203);
+    }
+
+    return c.json({
+      posts,
+    });
+  } catch (error) {
+    return c.json({
+      message: `Error occured ${error}`,
+    });
+  }
+};
+
+export const updatePost = async (c: Context) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const userId = c.get("userId");
+    const id = parseInt(c.req.param("id")!);
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId.userId,
+      },
+    });
+
+    if (!user) {
+      return c.body("User not found", 404);
+    }
+
+    const body: {
+      title: string;
+      body: string;
+    } = await c.req.json();
+
+    const { success } = postSchema.safeParse(body);
+    if (!success) {
+      return c.body("Invalid inputs", 411);
+    }
+
+    const post = await prisma.post.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!post) {
+      return c.body("No posts available", 404);
+    }
+
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: post.id,
+      },
+      data: {
+        title: body.title,
+        body: body.body,
+      },
+    });
+
+    return c.json({
+      updatedPost,
+    });
+  } catch (error) {
+    return c.json({
+      message: `Error occured ${error}`,
+    });
+  }
+};
+
+export const deletePost = async (c: Context) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const userId = c.get("userId");
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId.userId,
+    },
+  });
+
+  if (!user) {
+    return c.body("User not found", 404);
+  }
+
+  const id = parseInt(c.req.param("id")!);
+
+  const post = await prisma.post.delete({
+    where: {
+      userId: user.id,
+      id,
+    },
+  });
+
+  return c.json({
+    message: "Post deleted",
+    post,
+  });
 };
--- backend/src/routes/postRouter.ts
+++ backend/src/routes/postRouter.ts
@@ -1,8 +1,16 @@
 import { Hono } from "hono";
 import { authMiddleware } from "../middleware/middleware";
-import { getPosts, postPosts } from "../controler/postControler";
+import {
+  deletePost,
+  getPosts,
+  postPosts,
+  updatePost,
+} from "../controler/postControler";
 
 export const postRouter = new Hono();
 
 postRouter.get("/posts", authMiddleware, getPosts);
 postRouter.post("/create-posts", authMiddleware, postPosts);
+postRouter.get("/posts/:id", authMiddleware, getPosts);
+postRouter.put("/posts/:id", authMiddleware, updatePost);
+postRouter.delete("/posts/:id", authMiddleware, deletePost);
